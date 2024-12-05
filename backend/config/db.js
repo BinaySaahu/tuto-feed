568a14f81b16175f6bc31b1abf210ab47f753eb3
BinaySaahu <100080926+BinaySaahu@users.noreply.github.com>
2024-12-05 21:08:53 +0530

    fixed the strictQuery issue

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
+      mongoose.set('strictQuery', true);
       await mongoose.connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
+        // useNewUrlParser: true,
       });
       console.log("DataBase Connected");
     } catch (error) {
